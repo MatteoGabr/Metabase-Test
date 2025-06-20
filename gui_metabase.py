77adcb400c6c2fefff217b3981444d2f43b3ae69
MatteoGabr <matteo.gabrielli@edu.itspiemonte.it>
2025-06-20 16:20:42 +0200

    avvio metabase in gui

--- gui_metabase.py
+++ gui_metabase.py
@@ -2,6 +2,9 @@ import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
+import os
+
+os.system(r'Z: && cd Metabase && java -jar metabase.jar')
 
 app = QApplication(sys.argv)
 
