a41f1a38c8eedb7f27aed10a77c9abb76226c8c4
MatteoGabr <matteo.gabrielli@edu.itspiemonte.it>
2025-06-21 14:39:28 +0200

    attesa apertura Metabase

--- gui_metabase.py
+++ gui_metabase.py
@@ -1,20 +1,28 @@
 import sys
-from PySide6.QtWidgets import *
+from PySide6.QtWidgets import QApplication, QLabel
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QUrl, Qt, QTimer
 import subprocess
-import time
 
 mainPath = r"Z:/Metabase"
 command = 'java -jar metabase.jar'
-run = subprocess.Popen('start cmd /k '+ command,cwd=mainPath,shell=True)
-
-time.sleep(20)
+run = subprocess.Popen(['java', '-jar', 'metabase.jar'], cwd=mainPath, shell=False)
 
 app = QApplication(sys.argv)
 
+label = QLabel('Ciao! Aspetta Metabase!')
+label.setAlignment(Qt.AlignCenter)
+label.setStyleSheet("font-size: 50px;")
+label.showFullScreen()
+
+def switch_to_web():
+    label.close()
+    web.showFullScreen()
+
+QTimer.singleShot(15000, switch_to_web)
+
 web = QWebEngineView()
 web.load(QUrl("http://localhost:3000"))
-web.show()
+web.hide()
 
 sys.exit(app.exec())
